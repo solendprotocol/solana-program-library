4a8ef0e21bd07e80d345b40f072657fb1f7cabeb
0xripleys <0xripleys@solend.fi>
2022-06-27 17:47:45 -0400

    fml changed the wrong variable

--- token-lending/program/src/state/reserve.rs
+++ token-lending/program/src/state/reserve.rs
@@ -18,10 +18,10 @@ use std::{
 };
 
 /// Percentage of an obligation that can be repaid during each liquidation call
-pub const LIQUIDATION_CLOSE_FACTOR: u8 = 1;
+pub const LIQUIDATION_CLOSE_FACTOR: u8 = 20;
 
 /// Obligation borrow amount that is small enough to close out
-pub const LIQUIDATION_CLOSE_AMOUNT: u64 = 20;
+pub const LIQUIDATION_CLOSE_AMOUNT: u64 = 2;
 
 /// Lending market reserve state
 #[derive(Clone, Debug, Default, PartialEq)]
