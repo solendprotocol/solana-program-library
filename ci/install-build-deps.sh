be2983c1bef62f3e29e36d7ed686f819e86bfe61
0xripleys <0xripleys@solend.fi>
2022-11-21 13:32:06 -0500

    remove dep

--- ci/install-build-deps.sh
+++ ci/install-build-deps.sh
@@ -5,7 +5,7 @@ set -ex
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main"
 sudo apt-get update
-sudo apt-get install -y clang-7 --allow-unauthenticated
+# sudo apt-get install -y clang-7 --allow-unauthenticated
 sudo apt-get install -y openssl --allow-unauthenticated
 sudo apt-get install -y libssl-dev --allow-unauthenticated
 sudo apt-get install -y libssl1.1 --allow-unauthenticated
