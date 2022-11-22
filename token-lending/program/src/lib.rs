47652dc8b8ee11d0b7a9ca6de1fbaa7fd8054fb9
0xripleys <0xripleys@solend.fi>
2022-11-22 17:32:37 -0500

    delete pyth.rs

--- token-lending/program/src/lib.rs
+++ token-lending/program/src/lib.rs
@@ -6,10 +6,9 @@ pub mod entrypoint;
 pub mod error;
 pub mod instruction;
 pub mod math;
-pub mod processor;
-pub mod pyth;
-pub mod state;
 pub mod oracles;
+pub mod processor;
+pub mod state;
 
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
