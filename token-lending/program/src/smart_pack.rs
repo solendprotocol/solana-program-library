f3ae23441efe0a0e3f547520ebac0a41d26f2377
0xripleys <0xripleys@solend.fi>
2022-12-12 20:08:38 -0500

    refactoring stuff into a trait

--- token-lending/program/src/smart_pack.rs
+++ token-lending/program/src/smart_pack.rs
@@ -1,24 +1,95 @@
 //! Wrapper trait that can deserialize multiple versions of an object, and can re-alloc space if needed
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{account_info::AccountInfo, program_error::ProgramError};
+use solana_program::{
+    account_info::AccountInfo,
+    program_error::ProgramError,
+    program_pack::{IsInitialized, Pack},
+};
+
+use crate::{
+    error::LendingError,
+    state::{PROGRAM_VERSION, UNINITIALIZED_VERSION},
+};
 
 /// Wrapper trait that can deserialize multiple versions of an object, and can re-alloc space if
 /// needed
-pub trait SmartPack {
-    /// Type of item that is being packed/unpacked.
-    type Item;
+pub trait SmartPack<
+    V0: Pack + IsInitialized + From<V1>,
+    V1: BorshSerialize + BorshDeserialize + From<V0> + ValidateTag,
+>
+{
+    /// Find version of the object from the bytes representation
+    fn version(src: &[u8]) -> u8 {
+        match src.iter().next() {
+            // it's ok if the data buffer is _currently_ empty because we re-allocate in smart_pack
+            None => UNINITIALIZED_VERSION,
+            Some(v) => *v,
+        }
+    }
 
-    /// Find version of object using the serialized representation
-    fn version(src: &[u8]) -> u8;
+    /// Check if object is initialized from the bytes representation
+    fn is_initialized(src: &[u8]) -> bool {
+        Self::version(src) != UNINITIALIZED_VERSION
+    }
 
-    /// Check if a program account state is initialized
-    fn is_initialized(src: &[u8]) -> bool;
-
-    /// Unpack from slice and check if initialized
-    fn smart_unpack(src: &[u8]) -> Result<Self::Item, ProgramError>;
+    /// Unpack object from slice and check if initialized
+    fn smart_unpack(src: &[u8]) -> Result<V1, LendingError> {
+        match Self::version(src) {
+            UNINITIALIZED_VERSION => {
+                // msg!("Can't unpack an uninitialized object!");
+                Err(LendingError::FailedToDeserialize)
+            }
+            1 => match V0::unpack(src) {
+                Err(e) => Err(LendingError::FailedToDeserialize),
+                Ok(object) => Ok(object.into()),
+            },
+            2 => match V1::try_from_slice(src) {
+                Ok(object) => {
+                    object.validate_tag()?;
+                    Ok(object)
+                }
+                Err(e) => {
+                    // msg!("failed to borsh deserialize {:?}", e);
+                    Err(LendingError::FailedToDeserialize)
+                }
+            },
+            v => {
+                // msg!("Unimplemented version detected: {}", v);
+                Err(LendingError::FailedToDeserialize)
+            }
+        }
+    }
 
     /// Pack into slice. Re-alloc if the AccountInfo's data buffer is too small.
-    fn smart_pack(src: Self::Item, dst_account_info: &AccountInfo) -> Result<(), ProgramError>;
+    fn smart_pack(object: V1, dst_account_info: &AccountInfo) -> Result<(), ProgramError> {
+        // object.version = PROGRAM_VERSION;
+
+        match PROGRAM_VERSION {
+            1 => V0::pack(object.into(), &mut dst_account_info.try_borrow_mut_data()?),
+            2 => {
+                // serialize into a vector first
+                let serialized = object.try_to_vec().map_err(|e| {
+                    // msg!("failed to borsh serialize: {:?}", e);
+                    LendingError::FailedToSerialize
+                })?;
+
+                // 1. always realloc because try_from_slice will error on buffer len mismatches
+                // 2. zero-init out of paranoia but i don't think we actually need this
+                dst_account_info.realloc(serialized.len(), true)?;
+
+                // copy_from_slice panics if the sizes of the two slices don't match.
+                // in this case, we're guaranteed to not panic because we just realloc'd the account
+                let mut dst = dst_account_info.try_borrow_mut_data()?;
+                dst.copy_from_slice(&serialized);
+
+                Ok(())
+            }
+            v => {
+                // msg!("Unimplemented pack version detected: {}", v);
+                Err(LendingError::FailedToSerialize.into())
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
@@ -38,4 +109,10 @@ impl Default for AccountTag {
     fn default() -> Self {
         AccountTag::UnInitialized
     }
+}
+
+/// trait used to make sure the deserialized object has the correct tag.
+pub trait ValidateTag {
+    /// Returns a LendingError if the tag is incorrect.
+    fn validate_tag(&self) -> Result<(), LendingError>;
 }
--- token-lending/program/src/state/lending_market.rs
+++ token-lending/program/src/state/lending_market.rs
@@ -1,12 +1,10 @@
 use super::{lending_market_v0::LendingMarketV0, *};
 use crate::{
     error::LendingError,
-    smart_pack::{AccountTag, SmartPack},
+    smart_pack::{AccountTag, SmartPack, ValidateTag},
 };
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::AccountInfo, program_error::ProgramError, program_pack::Pack, pubkey::Pubkey,
-};
+use solana_program::pubkey::Pubkey;
 
 /// Lending market state
 #[derive(Clone, Debug, Default, PartialEq, BorshSerialize, BorshDeserialize)]
@@ -68,85 +66,16 @@ pub struct InitLendingMarketParams {
     pub switchboard_oracle_program_id: Pubkey,
 }
 
-impl SmartPack for LendingMarket {
-    type Item = LendingMarket;
-
-    fn version(src: &[u8]) -> u8 {
-        match src.iter().next() {
-            // it's ok if the data buffer is _currently_ empty because we re-allocate in smart_pack
-            None => UNINITIALIZED_VERSION,
-            Some(v) => *v,
-        }
-    }
-
-    fn is_initialized(src: &[u8]) -> bool {
-        Self::version(src) != UNINITIALIZED_VERSION
-    }
-
-    /// deserialize bytes into LendingMarket. This function can be called off-chain as well.
-    fn smart_unpack(src: &[u8]) -> Result<Self, ProgramError> {
-        match Self::version(src) {
-            UNINITIALIZED_VERSION => {
-                msg!("Can't unpack an uninitialized object!");
-                Err(LendingError::FailedToDeserialize.into())
-            }
-            1 => Ok(LendingMarketV0::unpack(src)?.into()),
-            2 => match LendingMarket::try_from_slice(src) {
-                Ok(lending_market) => match lending_market.tag {
-                    AccountTag::LendingMarket => Ok(lending_market),
-                    tag => {
-                        msg!("This account is not a lending market, it is a {:?}", tag);
-                        Err(LendingError::FailedToDeserialize.into())
-                    }
-                },
-                Err(e) => {
-                    msg!("failed to borsh deserialize {:?}", e);
-                    Err(LendingError::FailedToDeserialize.into())
-                }
-            },
-            v => {
-                msg!("Unimplemented version detected: {}", v);
-                Err(LendingError::FailedToDeserialize.into())
-            }
-        }
-    }
-
-    fn smart_pack(
-        mut lending_market: LendingMarket,
-        dst_account_info: &AccountInfo,
-    ) -> Result<(), ProgramError> {
-        lending_market.version = PROGRAM_VERSION;
-
-        match PROGRAM_VERSION {
-            1 => LendingMarketV0::pack(
-                lending_market.into(),
-                &mut dst_account_info.try_borrow_mut_data()?,
-            ),
-            2 => {
-                // serialize into a vector first
-                let serialized = lending_market.try_to_vec().map_err(|e| {
-                    msg!("failed to borsh serialize: {:?}", e);
-                    LendingError::FailedToSerialize
-                })?;
-
-                // 1. always realloc because try_from_slice will error on buffer len mismatches
-                // 2. zero-init out of paranoia but i don't think we actually need this
-                dst_account_info.realloc(serialized.len(), true)?;
-
-                // copy_from_slice panics if the sizes of the two slices don't match.
-                // in this case, we're guaranteed to not panic because we just realloc'd the account
-                let mut dst = dst_account_info.try_borrow_mut_data()?;
-                dst.copy_from_slice(&serialized);
-
-                Ok(())
-            }
-            v => {
-                msg!("Unimplemented pack version detected: {}", v);
-                Err(LendingError::FailedToSerialize.into())
-            }
+impl ValidateTag for LendingMarket {
+    fn validate_tag(&self) -> Result<(), LendingError> {
+        match self.tag {
+            AccountTag::LendingMarket => Ok(()),
+            _ => Err(LendingError::FailedToDeserialize),
         }
     }
 }
+
+impl SmartPack<LendingMarketV0, LendingMarket> for LendingMarket {}
 
 impl From<LendingMarketV0> for LendingMarket {
     fn from(lending_market_v0: LendingMarketV0) -> Self {
