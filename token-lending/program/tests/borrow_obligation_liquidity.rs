d9ddc68a74544f0ccd8fcf52eb6b28ea93ee5213
0xripleys <0xripleys@solend.fi>
2023-05-02 16:54:53 -0400

    tests for borrow/withdraw max on rate limiter

--- token-lending/program/tests/borrow_obligation_liquidity.rs
+++ token-lending/program/tests/borrow_obligation_liquidity.rs
@@ -1,6 +1,7 @@
 #![cfg(feature = "test-bpf")]
 
 use crate::helpers::solend_program_test::*;
+use solana_sdk::signature::Signer;
 
 use solend_program::math::TryDiv;
 mod helpers;
@@ -467,75 +468,132 @@ async fn test_fail_reserve_borrow_rate_limit_exceeded() {
         .unwrap();
 }
 
-// #[tokio::test]
-// async fn test_borrow_max_rate_limiter() {
-//     let (mut test, lending_market, reserves, obligations, users, lending_market_owner) =
-//         custom_scenario(
-//             &[
-//                 ReserveArgs {
-//                     mint: usdc_mint::id(),
-//                     config: test_reserve_config(),
-//                     liquidity_amount: 100_000 * FRACTIONAL_TO_USDC,
-//                     price: PriceArgs {
-//                         price: 10,
-//                         conf: 0,
-//                         expo: -1,
-//                         ema_price: 10,
-//                         ema_conf: 1,
-//                     },
-//                 },
-//                 ReserveArgs {
-//                     mint: wsol_mint::id(),
-//                     config: ReserveConfig {
-//                         loan_to_value_ratio: 50,
-//                         liquidation_threshold: 55,
-//                         fees: ReserveFees::default(),
-//                         optimal_borrow_rate: 0,
-//                         max_borrow_rate: 0,
-//                         ..test_reserve_config()
-//                     },
-//                     liquidity_amount: 100 * LAMPORTS_PER_SOL,
-//                     price: PriceArgs {
-//                         price: 10,
-//                         conf: 0,
-//                         expo: 0,
-//                         ema_price: 10,
-//                         ema_conf: 0,
-//                     },
-//                 },
-//             ],
-//             &[ObligationArgs {
-//                 deposits: vec![(usdc_mint::id(), 100 * FRACTIONAL_TO_USDC)],
-//                 borrows: vec![],
-//             }],
-//         )
-//         .await;
+#[tokio::test]
+async fn test_borrow_max_rate_limiter() {
+    let (mut test, lending_market, reserves, obligations, users, lending_market_owner) =
+        custom_scenario(
+            &[
+                ReserveArgs {
+                    mint: usdc_mint::id(),
+                    config: test_reserve_config(),
+                    liquidity_amount: 100_000 * FRACTIONAL_TO_USDC,
+                    price: PriceArgs {
+                        price: 10,
+                        conf: 0,
+                        expo: -1,
+                        ema_price: 10,
+                        ema_conf: 1,
+                    },
+                },
+                ReserveArgs {
+                    mint: wsol_mint::id(),
+                    config: ReserveConfig {
+                        loan_to_value_ratio: 50,
+                        liquidation_threshold: 55,
+                        fees: ReserveFees::default(),
+                        optimal_borrow_rate: 0,
+                        max_borrow_rate: 0,
+                        ..test_reserve_config()
+                    },
+                    liquidity_amount: 100 * LAMPORTS_PER_SOL,
+                    price: PriceArgs {
+                        price: 10,
+                        conf: 0,
+                        expo: 0,
+                        ema_price: 10,
+                        ema_conf: 0,
+                    },
+                },
+            ],
+            &[ObligationArgs {
+                deposits: vec![(usdc_mint::id(), 100 * FRACTIONAL_TO_USDC)],
+                borrows: vec![],
+            }],
+        )
+        .await;
 
-//     let wsol_reserve = &reserves[1];
-//     lending_market
-//         .update_reserve_config(
-//             &mut test,
-//             &lending_market_owner,
-//             &wsol_reserve,
-//             wsol_reserve.account.config,
-//             RateLimiterConfig {
-//                 window_duration: 20,
-//                 max_outflow: LAMPORTS_PER_SOL,
-//             },
-//             None,
-//         )
-//         .await
-//         .unwrap();
+    let wsol_reserve = &reserves[1];
+    lending_market
+        .update_reserve_config(
+            &mut test,
+            &lending_market_owner,
+            wsol_reserve,
+            wsol_reserve.account.config,
+            RateLimiterConfig {
+                window_duration: 20,
+                max_outflow: LAMPORTS_PER_SOL,
+            },
+            None,
+        )
+        .await
+        .unwrap();
 
-//     test.advance_clock_by_slots(1).await;
+    test.advance_clock_by_slots(1).await;
 
-//     lending_market.borrow_obligation_liquidity(
-//         &mut test,
-//         &wsol_reserve,
-//         &obligations[0],
-//         &users[0],
-//         &users[0],
-//         LAMPORTS_PER_SOL,
-//     )
+    let balance_checker = BalanceChecker::start(&mut test, &[wsol_reserve]).await;
 
-// }
+    lending_market
+        .borrow_obligation_liquidity(
+            &mut test,
+            wsol_reserve,
+            &obligations[0],
+            &users[0],
+            None,
+            u64::MAX,
+        )
+        .await
+        .unwrap();
+
+    // check token balances
+    let (balance_changes, _mint_supply_changes) =
+        balance_checker.find_balance_changes(&mut test).await;
+
+    let expected_balance_changes = HashSet::from([TokenBalanceChange {
+        token_account: wsol_reserve.account.liquidity.supply_pubkey,
+        mint: wsol_mint::id(),
+        diff: -(LAMPORTS_PER_SOL as i128),
+    }]);
+
+    assert_eq!(balance_changes, expected_balance_changes);
+
+    test.advance_clock_by_slots(100).await;
+
+    lending_market
+        .set_lending_market_owner_and_config(
+            &mut test,
+            &lending_market_owner,
+            &lending_market_owner.keypair.pubkey(),
+            RateLimiterConfig {
+                window_duration: 20,
+                max_outflow: 5, // $50
+            },
+        )
+        .await
+        .unwrap();
+
+    let balance_checker = BalanceChecker::start(&mut test, &[wsol_reserve]).await;
+
+    lending_market
+        .borrow_obligation_liquidity(
+            &mut test,
+            wsol_reserve,
+            &obligations[0],
+            &users[0],
+            None,
+            u64::MAX,
+        )
+        .await
+        .unwrap();
+
+    // check token balances
+    let (balance_changes, _mint_supply_changes) =
+        balance_checker.find_balance_changes(&mut test).await;
+
+    let expected_balance_changes = HashSet::from([TokenBalanceChange {
+        token_account: wsol_reserve.account.liquidity.supply_pubkey,
+        mint: wsol_mint::id(),
+        diff: -(LAMPORTS_PER_SOL as i128 / 2),
+    }]);
+
+    assert_eq!(balance_changes, expected_balance_changes);
+}
